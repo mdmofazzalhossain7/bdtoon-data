bd0e63df92a6d493e17e15cadabbeb5948004750
mdmofazzalhossain7 <mdmofazzalhossain789@gmail.com>
2023-06-17 13:05:20 +0600

    Created category data with images but not svg

---
+++ data/CategoryData.js
@@ -0,0 +1,217 @@
+export const CategoryData = [
+  {
+    id: 1,
+    title: "Action",
+    description:
+      "Action is a category that features high-energy, fast-paced sequences of physicality, combat, and excitement, aiming to captivate the audience with thrilling stunts and intense moments.",
+    imageUrl: "category/action.jpg",
+    icon: "",
+    url: "action",
+  },
+  {
+    id: 2,
+    title: "Adventure",
+    description:
+      "Adventure is a category that encompasses stories of thrilling journeys, quests, and explorations, often set in exotic or unknown locations, and filled with excitement, action, and personal growth.",
+    imageUrl: "category/adventure.jpg",
+    icon: "",
+    url: "adventure",
+  },
+  {
+    id: 3,
+    title: "Biography",
+    description:
+      "Adventure is a category that encompasses stories of thrilling journeys, quests, and explorations, often set in exotic or unknown locations, and filled with excitement, action, and personal growth.",
+    imageUrl: "category/biography.jpg",
+    icon: "",
+    url: "biography",
+  },
+  {
+    id: 4,
+    title: "Comedy",
+    description:
+      "Comedy entertains and provokes laughter through humorous situations, witty dialogue, and comedic performances, providing light-hearted and comedic entertainment.",
+    imageUrl: "category/comedy.jpg",
+    icon: "",
+    url: "comedy",
+  },
+  {
+    id: 5,
+    title: "Drama",
+    description:
+      "Drama explores intense emotional experiences, personal struggles, and complex human relationships, often delving into profound themes and eliciting strong emotional responses from the audience.",
+    imageUrl: "category/drama.jpg",
+    icon: "",
+    url: "drama",
+  },
+  {
+    id: 6,
+    title: "Eduactive",
+    description:
+      "The educative informs and enlightens the audience, providing valuable knowledge, insights, or perspectives on real-life subjects, historical events, scientific discoveries, social issues, or other educational topics.",
+    imageUrl: "category/educative.jpg",
+    icon: "",
+    url: "educative",
+  },
+  {
+    id: 7,
+    title: "Informative",
+    description:
+      "The informative provides factual and relevant information on specific topics to educate the audience and increase their understanding or awareness of subjects such as science, history, current events, or social issues.",
+    imageUrl: "category/informative.jpg",
+    icon: "",
+    url: "informative",
+  },
+  {
+    id: 8,
+    title: "Romance",
+    description:
+      "Romance revolves around love stories and romantic relationships, exploring themes of attraction, emotional connections, and the complexities of human affection and desire.",
+    imageUrl: "category/romance.jpg",
+    icon: "",
+    url: "romance",
+  },
+  {
+    id: 9,
+    title: "Fantasy",
+    description:
+      "Fantasy transports viewers to imaginative and often magical realms, featuring mythical creatures, supernatural elements, and extraordinary adventures beyond the boundaries of reality.",
+    imageUrl: "category/fantasy.jpg",
+    icon: "",
+    url: "fantasy",
+  },
+  {
+    id: 9,
+    title: "Science Fiction",
+    description:
+      "Science fiction explores futuristic concepts, advanced technology, and scientific possibilities beyond our current understanding, often blending scientific speculation with imaginative storytelling.",
+    imageUrl: "category/science-fiction.jpg",
+    icon: "",
+    url: "science-fiction",
+  },
+  {
+    id: 10,
+    title: "Horror",
+    description:
+      "Horror evokes fear, shock, and suspense through elements such as supernatural beings, monsters, psychological terror, and intense, chilling atmospheres.",
+    imageUrl: "category/horror.jpg",
+    icon: "",
+    url: "horror",
+  },
+  {
+    id: 11,
+    title: "Thriller",
+    description:
+      "Thriller keeps audiences on the edge of their seats through suspenseful storylines, unexpected twists, and intense anticipation, often involving thrilling pursuits, investigations, and high-stakes situations.",
+    imageUrl: "category/thriller.jpg",
+    icon: "",
+    url: "thriller",
+  },
+  {
+    id: 12,
+    title: "Mystery",
+    description:
+      "Mystery revolves around solving a crime, enigmatic situations, or puzzles, often engaging the audience in unraveling clues and uncovering the truth.",
+    imageUrl: "category/mystery.jpg",
+    icon: "",
+    url: "mystery",
+  },
+  {
+    id: 13,
+    title: "Documentary",
+    description:
+      "Documentary presents real-life subjects, events, or issues in a non-fictional and informative manner, aiming to educate, inform, and provide an objective perspective on various aspects of the world.",
+    imageUrl: "category/documentary.jpg",
+    icon: "",
+    url: "documentary",
+  },
+  {
+    id: 14,
+    title: "Historical",
+    description:
+      "Historical depicts stories, events, or characters set in specific periods of the past, often aiming to portray historical accuracy and provide insights into the cultural, social, or political context of the time.",
+    imageUrl: "category/historical.jpg",
+    icon: "",
+    url: "historical",
+  },
+  {
+    id: 15,
+    title: "Biography",
+    description:
+      "Biography portrays the life stories of real individuals, providing a glimpse into their personal journeys, achievements, struggles, and impact on society.",
+    imageUrl: "category/biograyphy.jpg",
+    icon: "",
+    url: "biography",
+  },
+  {
+    id: 16,
+    title: "Musical",
+    description:
+      "Musical incorporates song and dance numbers as integral parts of the storytelling, often featuring characters expressing their emotions and advancing the plot through musical performances.",
+    imageUrl: "category/musical.jpg",
+    icon: "",
+    url: "musical",
+  },
+  {
+    id: 17,
+    title: "Crime",
+    description:
+      "Crime focuses on the exploration of criminal activities, investigations, and the pursuit of justice, often delving into the complexities of law enforcement, organized crime, and the motivations of both criminals and those seeking to bring them to justice.",
+    imageUrl: "category/crime.jpg",
+    icon: "",
+    url: "crime",
+  },
+  {
+    id: 18,
+    title: "Family",
+    description:
+      "Family offers content suitable for all members of the family, often emphasizing themes of love, unity, and positive values, providing entertainment that can be enjoyed by viewers of different ages.",
+    imageUrl: "category/family.jpg",
+    icon: "",
+    url: "family",
+  },
+  {
+    id: 19,
+    title: "War",
+    description:
+      "War portrays stories set during wartime, capturing the experiences of soldiers, the impact on societies, and human struggles, often exploring themes of sacrifice, bravery, and the devastating consequences of armed conflicts.",
+    imageUrl: "category/war.jpg",
+    icon: "",
+    url: "war",
+  },
+  {
+    id: 20,
+    title: "Supernatural",
+    description:
+      "Supernatural involves paranormal or otherworldly elements, such as ghosts, vampires, or supernatural powers, often exploring mysterious and unexplained phenomena beyond the realm of natural laws.",
+    imageUrl: "category/supernatural.jpg",
+    icon: "",
+    url: "supernatural",
+  },
+  {
+    id: 21,
+    title: "Slice of Life",
+    description:
+      "The slice of life portrays realistic, everyday situations and events in the lives of ordinary people.",
+    imageUrl: "category/slice-of-life.jpg",
+    icon: "",
+    url: "slice-of-life",
+  },
+  {
+    id: 22,
+    title: "Vlog",
+    description:
+      "The vlog category provides documenting and sharing personal experiences, activities, or thoughts of individuals through a video blog format.",
+    imageUrl: "category/vlog.jpg",
+    icon: "",
+    url: "vlog",
+  },
+  {
+    id: 23,
+    title: "Others",
+    description: "All the other categories are here in this section",
+    imageUrl: "category/others.jpg",
+    icon: "",
+    url: "others",
+  },
+];
